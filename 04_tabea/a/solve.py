dad1c7a2d5f6bda98fedfe0a4af2c63d3431a66c
tabge <stud.tabea@gmail.com>
2025-12-04 17:33:31 +0100

    Tabea Day 4a

---
+++ 04_tabea/a/solve.py
@@ -0,0 +1,47 @@
+import os, sys
+
+PAPER = "@"
+EMPTY = "."
+
+def solve(filename):
+    with open(filename, 'r') as f:
+        inp = f.read().strip().split("\n")
+        inp = [list(line) for line in inp]
+
+    accessibility = inp
+
+    count_accessible_paper = 0
+    for y_index, y in enumerate(inp):
+        for x_index, x in enumerate(inp[y_index]):
+            if inp[y_index][x_index] == PAPER:
+                n_neigbours = count_neighbour(y_index, x_index, inp)
+                if n_neigbours < 4:
+                    count_accessible_paper += 1
+                    accessibility[y_index][x_index] = x
+                    print("accessible paper at: ", y_index, x_index, " with ", n_neigbours, " neigbours")
+    return count_accessible_paper
+
+def count_neighbour(i,j,grid):
+    n_neigbours = 0
+    directions = [  (-1, -1),   (-1, 0),    (-1, 1),
+                    (0, -1),                (0, 1),
+                    (1, -1),    (1, 0),     (1, 1)]
+    for d in directions:
+        di, dj = d
+        ni, nj = i + di, j + dj
+        if 0 <= ni < len(grid) and 0 <= nj < len(grid[i]):
+            if grid[ni][nj] == PAPER:
+                n_neigbours += 1
+    return n_neigbours
+
+if __name__ == "__main__":
+    if len(sys.argv) >= 2:
+        filepath = sys.argv[1]
+    else:
+        filepath = "input.txt"
+
+    if os.path.isfile(filepath):
+        results = solve(filepath)
+        print(results)
+    else:
+        print("There is no such file")
